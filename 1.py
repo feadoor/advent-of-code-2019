82c15f5d089d54eee72476883847e70eb11b2fda
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-12-01 05:09:28 +0000

    Day 1 solution

---
+++ 1.py
@@ -0,0 +1,19 @@
+def get_module_weights():
+    with open('data/1.txt', 'r') as file:
+        return [int(line) for line in file]
+
+def fuel_for_mass(mass):
+    return mass // 3 - 2
+
+def recursive_fuel_for_mass(mass):
+    fuel = fuel_for_mass(mass)
+    return 0 if fuel <= 0 else fuel + recursive_fuel_for_mass(fuel)
+
+def get_total_fuel_requirements():
+    return sum(map(fuel_for_mass, get_module_weights()))
+
+def get_recursive_fuel_requirements():
+    return sum(map(recursive_fuel_for_mass, get_module_weights()))
+
+print(get_total_fuel_requirements())
+print(get_recursive_fuel_requirements())
