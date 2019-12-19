bd10f76441a345daef3354d8e330d0e074fb5815
Sam Cappleman-Lynes <sam.capplemanlynes@gmail.com>
2019-12-19 10:02:56 +0000

    Speed up Python Day 19

--- python/19.py
+++ python/19.py
@@ -14,23 +14,24 @@ def is_beam(x, y):
     vm.send(y)
     return vm.receive() == 1
 
-def get_first_bounding_box(dx, dy):
-    starts, ends = [], []
-    for x in count(0):
-
-        start_y = 0 if not starts else starts[-1]
+def get_count(width, height):
+    start_y, count = 0, 0
+    for x in range(0, width):
         while not is_beam(x, start_y): start_y += 1
-        starts.append(start_y)
+        for y in range(start_y, height):
+            if is_beam(x, y): count += 1
+            else: break
+    return count
 
-        for y in count(start_y + 1):
-            if not is_beam(x, y): break
-        ends.append(y - 1)
-
-        if (x >= dx - 1 and ends[x] - starts[x] >= dy - 1 and starts[x] + dy - 1 <= ends[x - dx + 1]):
-            return (x - dx + 1, starts[x])
+def get_first_bounding_box(dx, dy):
+    y = 0
+    for x in count(0):
+        while not is_beam(x, y): y += 1
+        if not is_beam(x, y + dy - 1): continue
+        if x >= dx - 1 and is_beam(x - dx + 1, y + dy - 1): return (x - dx + 1, y)
 
 def part1():
-    return sum(is_beam(x, y) for x in range(50) for y in range(50))
+    return get_count(50, 50)
 
 def part2():
     (x, y) = get_first_bounding_box(100, 100)
